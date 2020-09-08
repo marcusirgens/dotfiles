f4c2c1703e5200a21b7727efad0a41aede52d698
Marcus Pettersen Irgens <marcus.pettersen.irgens@gmail.com>
2020-09-08 13:12:11 +0200

    Check for aws-vault

--- aws-vault/aws-shell.zsh
+++ aws-vault/aws-shell.zsh
@@ -1,4 +1,11 @@
 aws-shell () {
+    if ! command -v aws-vault; then
+      >&2 echo "aws-vault not found in PATH"
+      return 1;
+    fi;
+
+    return;
+
     local TIME=""
     if [ "$2" != "" ]; then
         TIME=$2
