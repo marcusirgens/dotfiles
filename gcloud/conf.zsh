ef3a5334455a70564f9c7e9e578aadef14ac075d
Marcus Pettersen Irgens <m@mrcus.dev>
2022-07-11 21:23:28 +0200

    Use the gke gcloud auth plugin

--- gcloud/conf.zsh
+++ gcloud/conf.zsh
@@ -5,3 +5,6 @@ if [ -f "$GCLOUD_SRC/path.zsh.inc" ]; then source "$GCLOUD_SRC/path.zsh.inc"; fi
 
 # The next line enables shell command completion for gcloud.
 if [ -f "$GCLOUD_SRC/completion.zsh.inc" ]; then source "$GCLOUD_SRC/completion.zsh.inc"; fi
+
+# https://cloud.google.com/blog/products/containers-kubernetes/kubectl-auth-changes-in-gke
+export USE_GKE_GCLOUD_AUTH_PLUGIN="True"
