5f5b10af4d93e292cf5b4e1bf674542455034c0b
Marcus Pettersen Irgens <m@mrcus.dev>
2022-07-11 23:07:48 +0200

    Don't always install Rosetta 2

--- system/install.zsh
+++ system/install.zsh
@@ -4,8 +4,10 @@ source "$DOTFILE_DIR/install/functions.zsh"
 # Architecture specific
 case "$(uname -m)" in
       "arm64" )
-        echobox "You may now be prompted for your sudo password to install Rosetta 2."
-        sudo softwareupdate --install-rosetta --agree-to-license
+        if [ ! -f "/Library/Apple/usr/share/rosetta/rosetta" ]; then
+          echobox "You may now be prompted for your sudo password to install Rosetta 2."
+          sudo softwareupdate --install-rosetta --agree-to-license
+        fi
         ;;
     esac
 
