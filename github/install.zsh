df57129591c8f2b806017daebf9cefa1a17e92c7
Marcus Pettersen Irgens <m@mrcus.dev>
2022-07-11 23:56:29 +0200

    Add warning to GitHub CLI installer

--- github/install.zsh
+++ github/install.zsh
@@ -1,3 +1,6 @@
+# Fetch common functions
+source "$DOTFILE_DIR/install/functions.zsh"
+
 set -eu;
 
 GITHUB_DIR="$DOTFILE_DIR/github"
@@ -10,5 +13,6 @@ if [ -f "$DOTFILE_DIR/secrets.sh" ]; then
 fi;
 
 if [ ! -z "$GITHUB_TOKEN_REF" ]; then
+  echobox "1password will now attempt to authenticate to get your GitHub token."
   op read "$GITHUB_TOKEN_REF" | gh auth login --with-token
 fi
