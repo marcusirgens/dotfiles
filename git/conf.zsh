36ccb5c9eb7aa4c76c6a69311b820c82f0ba7512
Marcus Pettersen Irgens <m@mrcus.dev>
2022-07-11 22:47:33 +0200

    Start collecting Git aliases

---
+++ git/conf.zsh
@@ -0,0 +1,6 @@
+alias glog='git log -n 20 --all --pretty=format:"%Cblue%h%Creset %s (%Cgreen%ad%Creset)" --abbrev-commit --date=relative'
+alias gco="git checkout"
+alias gcob="git checkout -b"
+
+alias gsw="git switch"
+alias gswc="git switch -c"
