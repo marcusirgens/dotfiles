f41d0586b24780bcf92c41200230036ed0d8d313
Marcus Pettersen Irgens <m@mrcus.dev>
2022-08-22 16:28:38 +0200

    Support intel macs

--- dot_config/fish/conf.d/brew.fish
+++ dot_config/fish/conf.d/brew.fish
@@ -2,3 +2,16 @@ if test -f /opt/homebrew/bin/brew
     and type -q /opt/homebrew/bin/brew
     eval (/opt/homebrew/bin/brew shellenv)
 end
+
+switch "$(uname -s)"
+    case "Darwin"
+        # We could use brew --prefix openjdk, but it fails when java is not installed.
+        if type -q test -f /opt/homebrew/bin/brew
+            and type -q /opt/homebrew/bin/brew
+            eval (/opt/homebrew/bin/brew shellenv)
+        else if type -q test -f /usr/local/bin/brew
+            and type -q /usr/local/bin/brew
+            eval (/opt/homebrew/bin/brew shellenv)
+        end
+end
+
