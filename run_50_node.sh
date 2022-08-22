943524b336ab77da57109a8fd9a9edb31a566752
Marcus Pettersen Irgens <m@mrcus.dev>
2022-08-22 17:56:16 +0200

    Fix invalid conditional in node config

--- run_50_node.sh
+++ run_50_node.sh
@@ -3,7 +3,7 @@ set -eu;
 
 export N_PREFIX="{{.chezmoi.homeDir}}/{{.node.ndir}}"
 
-if ! command -v n > /dev/null; then
+if command -v n > /dev/null; then
   n install lts --quiet --download
   n install latest --quiet --download
 fi
