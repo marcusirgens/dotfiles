770cee19ea09b4a44041fc28b4cddb4f156615fa
Marcus Pettersen Irgens <m@mrcus.dev>
2022-08-22 15:47:46 +0200

    Add java configuration

---
+++ dot_config/fish/conf.d/java.fish
@@ -0,0 +1,9 @@
+switch $(uname -s)
+    case "Darwin"
+        # We could use brew --prefix openjdk, but it fails when java is not installed.
+        if type -q brew
+            and test -d "$(brew --prefix)/opt/openjdk/bin"
+            fish_add_path "$(brew --prefix)/opt/openjdk/bin/"
+        end
+end
+
