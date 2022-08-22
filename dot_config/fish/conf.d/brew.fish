78b58cd93bb27dea263fbf22e6f6416c7415f8cc
Marcus Pettersen Irgens <m@mrcus.dev>
2022-08-22 17:17:21 +0200

    Fix brew path in brew script

--- dot_config/fish/conf.d/brew.fish
+++ dot_config/fish/conf.d/brew.fish
@@ -1,8 +1,3 @@
-if test -f /opt/homebrew/bin/brew
-    and type -q /opt/homebrew/bin/brew
-    eval (/opt/homebrew/bin/brew shellenv)
-end
-
 switch "$(uname -s)"
     case "Darwin"
         # We could use brew --prefix openjdk, but it fails when java is not installed.
@@ -11,7 +6,7 @@ switch "$(uname -s)"
             eval (/opt/homebrew/bin/brew shellenv)
         else if type -q test -f /usr/local/bin/brew
             and type -q /usr/local/bin/brew
-            eval (/opt/homebrew/bin/brew shellenv)
+            eval (/usr/local/bin/brew shellenv)
         end
 end
 
