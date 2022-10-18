937e3b9ed52a4a1b2d94fc79a38bfbad7b39fb4f
Marcus Pettersen Irgens <m@mrcus.dev>
2022-10-18 11:14:50 +0200

    Use abbr for git commands

--- dot_config/fish/conf.d/git-alias.fish
+++ dot_config/fish/conf.d/git-alias.fish
@@ -1,9 +1,9 @@
-alias glog='git log -n 20 --all --pretty=format:"%Cblue%h%Creset %s (%Cgreen%ad%Creset)" --abbrev-commit --date=relative'
-alias gco="git checkout"
-alias gcob="git checkout -b"
+abbr --add glog git log -n 20 --all --pretty=format:\"%Cblue%h%Creset %s (%Cgreen%ad%Creset)\" --abbrev-commit --date=relative
+abbr --add gco git checkout
+abbr --add gcob git checkout -b
 
-alias gs="git status"
-alias gsw="git switch"
-alias gswc="git switch -c"
-alias grb="git rebase"
-alias grbi="git rebase --interactive"
+abbr --add gs git status
+abbr --add gsw git switch
+abbr --add gswc git switch -c
+abbr --add grb git rebase
+abbr --add grbi git rebase --interactive
